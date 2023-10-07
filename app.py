3fe1a522ce6ba1cc906542b0d51f6d3714156db5
Thierry Decae <thierrydecae@hotmail.com>
2023-10-07 17:44:22 +0000

    fixed typo in app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def predict(prompt):
 
 with gr.Blocks() as demo:
     textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn = predict, inputs = textbox, output = "text")
+    gr.Interface(fn = predict, inputs = textbox, outputs = "text")
 
 demo.launch()
